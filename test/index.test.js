a2712abfc532556ef5dc1a1784559f6c09e84837
施俊羽 <tom19960222@gmail.com>
2019-04-08 23:00:36 +0800

    Add attachNode function

--- test/index.test.js
+++ test/index.test.js
@@ -91,22 +91,21 @@ test('Should return all its parents of a node', () => {
   expect(parentNodesOfSubNode1).toHaveLength(4);
 });
 
-test('Should change parent if node existed', () => {
+test('Can insert all data to root node before relationship is known', () => {
   const rootNode = new Node({ id: 1, data: { name: 'test1' } });
   const subNode1 = new Node({ id: 2, data: { name: 'test2' } });
   const subSubSubNode1 = new Node({ id: 5, data: { name: 'test5' } });
   const subSubSubNode2 = new Node({ id: 6, data: { name: 'test5' } });
 
-  // Example data (category_id, sub_category_id): (2, 5), (2, 6), (1, 2)
   const tree = new Tree();
-  tree.addNode(null, subNode1);
-  tree.addNode(2, subSubSubNode1);
-
-  tree.addNode(null, subNode1);
-  tree.addNode(2, subSubSubNode2);
-
   tree.addNode(null, rootNode);
-  tree.addNode(1, subNode1);
+  tree.addNode(null, subNode1);
+  tree.addNode(null, subSubSubNode1);
+  tree.addNode(null, subSubSubNode2);
+
+  tree.attachNode(5, 2);
+  tree.attachNode(6, 2);
+  tree.attachNode(2, 1);
 
   expect(tree.rootNodes).toEqual([rootNode]);
   expect(rootNode.childNodes).toEqual([subNode1]);
